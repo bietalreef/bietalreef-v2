cb8c269c822af3717948dfa07bf0cdda51ccc080
bietalreef <178828762+bietalreef@users.noreply.github.com>
2025-11-06 08:33:41 -0500

    Revert "Add Bietalreef custom CSS with correct colors (#2D5016)"
    This reverts commit fcdaea1476758c7f9ce56da119a77aac98fd3c84.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,6 @@ import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
 import "./index.css";
-import "./styles/bietalreef.css";
 
 const queryClient = new QueryClient();
 
